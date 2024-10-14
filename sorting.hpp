4b2ec30d2d9937f384d3ee9860079e8624a797dc
amaya-j <164046047+amaya-j@users.noreply.github.com>
2024-10-13 21:44:50 -0400

    Add files via upload

---
+++ sorting.hpp
@@ -0,0 +1,11 @@
+#ifndef SORTING_HPP
+#define SORTING_HPP
+
+// Insertion Sort
+template <typename T>
+void insertion_sort(T *arr, long n);
+
+// Merge Sort
+template <typename T>
+T* merge_sort(T *arr, long n);
+
